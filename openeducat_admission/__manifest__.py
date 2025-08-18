6c4f8edd2cbeb2ada00ebc3a429999f5519f2037
Ignacio J. Ortega <nacho.ortega@gmail.com>
2025-08-18 08:52:49 +0200

    update version numbers to 16.0.0.0.1 for OpenEduCat modules

--- openeducat_admission/__manifest__.py
+++ openeducat_admission/__manifest__.py
@@ -21,7 +21,7 @@
 
 {
     'name': "OpenEduCat Admission",
-    'version': '12.0.0.1',
+    'version': '16.0.0.0.1',
     'license': 'LGPL-3',
     'category': 'Education',
     'sequence': 3,
--- openeducat_support/__manifest__.py
+++ openeducat_support/__manifest__.py
@@ -22,7 +22,7 @@
     'name': 'OpenEduCat Support',
     'category': 'Planner',
     'summary': 'Help to configure OpenEduCat',
-    'version': '12.0.0.1',
+    'version': '16.0.0.0.1',
     'license': 'LGPL-3',
     "sequence": 3,
     'author': 'Tech Receptives',
